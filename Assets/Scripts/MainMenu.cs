154f1690932f7b30d421854954630a22b5a1db05
ioelion <rughnar@protonmail.com>
2024-12-08 21:57:02 -0300

    full menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -13,4 +13,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 }
